e03a22a18b5cc2feb3e04527ae1662adff2360b7
moriyahaddad <128741228+moriyahaddad@users.noreply.github.com>
2023-06-29 17:43:11 +0300

    Update RadialGeometry.java

--- src/geometries/RadialGeometry.java
+++ src/geometries/RadialGeometry.java
@@ -1,13 +1,26 @@
 package geometries;
 
-public abstract class RadialGeometry  extends Geometry  {
+/**
+ * Abstract class representing a radial geometry, which is a geometry with a specific radius.
+ * It extends the Geometry class.
+ */
+public abstract class RadialGeometry extends Geometry {
 
- protected double radius;
+    protected double radius;
 
- public RadialGeometry(double radius) {
-	 this.radius = radius;}
- 
- public double getRadius() {
-		return radius;}
+    /**
+     * Constructs a RadialGeometry object with the given radius.
+     * @param radius The radius of the radial geometry.
+     */
+    public RadialGeometry(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * Returns the radius of the radial geometry.
+     * @return The radius value.
+     */
+    public double getRadius() {
+        return radius;
+    }
 }
-
